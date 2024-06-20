be25ef21112df3c8e21bf0b00d4f0796fbf04df6
tiposamuel <tipo-samuel@hotmail.com>
2024-06-20 18:37:50 -0400

    feat: add logic to determine palindrome

--- script.js
+++ script.js
@@ -15,3 +15,56 @@ month = month < 10 ? '0' + month : month
 // insertar la fecha actual en el DOM
 $date.innerHTML = `${day}//<br>${month}//<br>${year}`
 
+// ---------------------  Palindrome  ---------------------
+
+// identifica los elementos del DOM para ser manipulados con JS
+const $checkButton = document.getElementById('check-btn')
+const $palindromeInput = document.getElementById('text-input')
+const $result = document.getElementById('result')
+
+// función para validar si el input del usuario es o no un palíndromo
+const checkPalindrome = (input) => {
+  // almacena el input del usuario
+  const originalInput = input
+
+  // comprueba si el usuario realmente ingreso texto en el input
+  if (!input) {
+    alert('Please input a value')
+    return
+  }
+
+  // limpia el resultado anterior
+  $result.replaceChildren()
+
+  // remueve todos los símbolos de puntuación del string y transforma a minúsculas
+  let cleanedString = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase()
+  // inicializa la variable del mensaje del resultado a mostrar
+  let resultMessage
+
+  // comprueba si el string es un palíndromo y devuelve un mensaje con el resultado
+  if (cleanedString === [...cleanedString].reverse().join('')) {
+    resultMessage = `${originalInput} is a palindrome`
+  } else {
+    resultMessage = `${originalInput} is not a palindrome`
+  }
+
+  // crea un elemento span donde se inserta el mensaje con el resultado
+  let spanMessage = $result.appendChild(document.createElement('span'))
+  // vuelve el resultado visible en el dom
+  $result.style.visibility = 'visible'
+  // retorna el mensaje
+  return (spanMessage.textContent = resultMessage)
+}
+
+// eventos
+$checkButton.addEventListener('click', () => {
+  checkPalindrome($palindromeInput.value)
+  $palindromeInput.value = ''
+})
+
+$palindromeInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    checkPalindrome($palindromeInput.value)
+    $palindromeInput.value = ''
+  }
+})
